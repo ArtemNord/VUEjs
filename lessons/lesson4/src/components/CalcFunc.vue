70243bd939a76cb6b3c53225b5d3f6e9e6f26941
Nord.A <mr.nordakov@gmail.com>
2024-03-04 16:29:12 +0300

    lesson 4 & 5

---
+++ lessons/lesson4/src/components/CalcFunc.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input type="text">
+    <input type="text">
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: 'CalcFunc',
+  dara() {
+    
+    return {
+      
+    };
+  },
+  methods : {
+
+  }
+};
+</script>
+
